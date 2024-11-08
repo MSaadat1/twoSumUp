614dda1dbe3dcb7b8a6ce4f12a25dc4009345945
Mezhgan Saadat <mezhgan@mezhgans-air.mynetworksettings.com>
2024-11-07 20:35:53 -0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-//remove duplicated elements from a list of arrays
-//using new Set to remove duplicate
-function removeDupes(array){
-    const set = new Set(array);
-    return Array.from(set);
+// function with two arguments
+// the first argument is an array of numbers
+// the second argument is one number
+// function returns the sum up of two numbers from the first
+// argument to be equal to the second argument which is one number
+// return the index of those two numbers
+// we are using new Map() for time complicity
+// loop to access each value of array
+function twoSumOptimized(array, target){
+    const map = new Map();
+    for(let i = 0; i < array.length; i++){
+        let index = target - array[i];
+        if(map.has(index)) return [map.get(index), i];
+        map.set(array[i], i)
+    }
+    return null;
+
 }
-console.log(removeDupes([1,1,2,2,2,4,4,3,3,5,5,6]));
+console.log(twoSumOptimized([2,3,4,2], 6));
